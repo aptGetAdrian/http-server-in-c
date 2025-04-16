0312425a70ee51daf6aaade5f0fbe53659bb925c
Adrian <cvetkoadrian@gmail.com>
2025-04-16 15:46:33 +0200

    updated .gitignore file

--- server.c
+++ server.c
@@ -13,7 +13,51 @@
 
 /*
 TODO:
-vrednosti v status code arrayi se ne prenesejo. ocitno je pointer issue. server posle 500 internal server error
+
+
+1. create functionaly for reading the request body.
+2. finish the codecrafters course.
+
+// Inside the thread:
+pthread_mutex_lock(&status_mutex);
+// Modify status_codes safely
+pthread_mutex_unlock(&status_mutex);
+
+pthread_mutex_t status_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+AFTER: optimizations:
+use this chatgpt chat as refference: 
+https://chatgpt.com/c/67de05f2-7c08-8002-935d-37cf566f76fe
+
+
+use the thread pool file to implement a thread pool for better safety and easier use.
+
+namesto vectora probej array stringov. sigurno se da
+https://stackoverflow.com/questions/1088622/how-do-i-create-an-array-of-strings-in-c
+
+https://www.geeksforgeeks.org/array-of-strings-in-c/
+
+
+
+naredi thread pool 
+
+2️⃣ Race Condition on status_codes
+args->status_codes = status_codes; suggests that status_codes is shared across multiple threads.
+If multiple threads modify it without synchronization (e.g., mutex), you may have race conditions.
+✅ Fix: If status_codes is read-only, it’s safe. If it’s modified, use a mutex:
+
+c
+Copy
+Edit
+pthread_mutex_t status_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Inside the thread:
+pthread_mutex_lock(&status_mutex);
+// Modify status_codes safely
+pthread_mutex_unlock(&status_mutex);
+
+
+
 
 
 
@@ -31,6 +75,53 @@ typedef struct {
     char** status_codes;
 	int* clientFd;
 } handlerArg;
+
+
+typedef struct {
+	const char* extention;
+	const char* typeString;
+
+} fileType;
+
+char *concatStrings (const char *s1, const char *s2) {
+    char *temp = malloc(strlen(s1) + strlen(s2) + 1);
+    if (!temp) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+    strcpy(temp, s1);
+    strcat(temp, s2);
+    return temp;
+}
+
+
+fileType fileTypes[] = {
+	{ ".html", "text/html" },
+    { ".css", "text/css" },
+    { ".js", "application/javascript" },
+    { ".png", "image/png" },
+    { ".jpg", "image/jpeg" },
+    { ".jpeg", "image/jpeg" },
+    { ".gif", "image/gif" },
+    { ".svg", "image/svg+xml" },
+    { ".json", "application/json" },
+    { ".pdf", "application/pdf" }
+};
+
+
+const char* getFileType (const char* path) {
+	const char* temp = strrchr(path, '.');
+
+	if (!temp) return "application/octet-stream";	
+
+	for (size_t i = 0; i < 10; i++) {
+		if (strcmp(temp, fileTypes[i].extention) == 0) {
+			return fileTypes[i].typeString;
+		}
+	}
+	return "application/octet-stream";
+}
+
 
 //typedef struct  {
 //    char* code;
@@ -72,7 +163,7 @@ char* bulildStatusCodeReply(int code, char* status_codes[NUM_STATUS_CODES]) {
 			return "HTTP/1.1 500 Internal Server Error\r\n\r\n";
 	} 
 
-	char* pchar = malloc(sizeof(char) * 60);
+	char* pchar = malloc(sizeof(char)*  60);
 	if (pchar == NULL) {
 		printf("Memory allocation failed: %s...\n", strerror(errno));
 		exit(1);
@@ -81,7 +172,7 @@ char* bulildStatusCodeReply(int code, char* status_codes[NUM_STATUS_CODES]) {
 	return pchar;
 }
 
-long geFileSize(FILE* pointer) {
+long getFileSize(FILE* pointer) {
 	long size;
 	fseek(pointer, 0L, SEEK_END);
 	size = ftell(pointer);
@@ -89,16 +180,16 @@ long geFileSize(FILE* pointer) {
 	return size;
 }
 
-void* clientHandling (void *arguments) {
+void* clientHandling (void* arguments) {
 	char buffer[16384] = {0};
 	int counter = 0;
 	requestElement requestLine[3];
 	requestElement* vec = vector_create(); 
-	char *saveptr1, *saveptr2;
-	char* replyStatus, *reply;
+	char* saveptr1, *saveptr2;
+	char* replyStatus, *reply, *postData;
 
-	handlerArg *args = arguments;
-	int clientFd = *args->clientFd;
+	handlerArg* args = arguments;
+	int clientFd =* args->clientFd;
 	char** status_codes = args->status_codes;
 
 	//recieve a buffer of 4096 bytes
@@ -110,7 +201,7 @@ void* clientHandling (void *arguments) {
 
 
 	//splitting buffer2 by delim = " "
-	char *token = strtok_r(buffer, " ", &saveptr1);
+	char* token = strtok_r(buffer, " ", &saveptr1);
 	requestLine[0] = (requestElement) {"Method", token};
 	token = strtok_r(NULL, " ", &saveptr1);
 	requestLine[1] = (requestElement) {"Target", token};
@@ -132,34 +223,55 @@ void* clientHandling (void *arguments) {
 		char* value = "";
 		char* token2 = token;
 
-		key = strtok_r(token2, " ", &saveptr2);
-		value = strtok_r(NULL, " ", &saveptr2);
-		
-		if (key[strlen(key)-2] == ':') {
-			key[strlen(key)-2] = '\0';
-		} else {
-			key[strlen(key)-1] = '\0';
-		}
 		
 		
 
-		requestElement test = {key, value};
-		
-		vector_add(&vec, test);
-		//printf("%s: %s\n", vec[0].key, vec[0].value);
-		counter++;
+		//already checks the next token
+		//if it's empty and the request method is POST, it takes the last value and puts into the POSdata variable,
+		//which is used later
+		//otherwise, we split the second token again to get the key and value of the request header and store it
 
 		token = strtok_r(NULL, "\r\n", &saveptr1);
+
+		if (token == NULL && strcmp(requestLine[0].value, "POST") == 0) {
+			postData = malloc(sizeof(char)*  strlen(token2));
+			memcpy(postData, token2, strlen(token2));
+
+		} else {
+			key = strtok_r(token2, " ", &saveptr2);
+			value = strtok_r(NULL, " ", &saveptr2);
+
+			if (key[strlen(key)-2] == ':') {
+				key[strlen(key)-2] = '\0';
+			} else {
+				key[strlen(key)-1] = '\0';
+			}
+
+			requestElement test = {key, value};
+		
+			vector_add(&vec, test);
+		}
+
+
+		
+		//printf("%s: %s\n", vec[0].key, vec[0].value);
+		//counter++;
+
+		
+		//printf("token: %s\n", token);
 	}
+
+	
+
+	//printf("%s\n", token);
 
 
 	//DONE PARSING THE HTTP REQUEST
-	//for now only the request line and the headers. will do the body later
 
 	if (strcmp(requestLine[0].value, "GET") == 0 && strcmp(requestLine[1].value, "") != 0) {
 		int bytes_sent;
 		if (strncmp(requestLine[1].value, "/echo/", 6) == 0) {
-			char *outString = requestLine[1].value + 6;
+			char* outString = requestLine[1].value + 6;
 
 			if (status_codes[200]) {
 				replyStatus = bulildStatusCodeReply(200, status_codes);
@@ -168,14 +280,14 @@ void* clientHandling (void *arguments) {
 			}
 
 
-			reply = malloc(sizeof(char) * 1024);
+			reply = malloc(sizeof(char)*  1024);
 			snprintf(reply, 1024, "%s%s%ld%s%s\n", replyStatus, "Content-Type: text/plain\r\nContent-Length: ", (strlen(outString) + 1), "\r\n\r\n", outString);
 			
 
 			bytes_sent = send(clientFd, reply, strlen(reply), 0);
 			free(reply);
 		} else if (strcmp(requestLine[1].value, "/user-agent") == 0) {
-			char *outString;
+			char* outString;
 
 			for (size_t i = 0; i < vector_size(vec); i++) {
 				if (strcmp(vec[i].key, "User-Agent") == 0) {
@@ -183,6 +295,7 @@ void* clientHandling (void *arguments) {
 				}
 			}
 			
+
 			if (status_codes[200]) {
 				replyStatus = bulildStatusCodeReply(200, status_codes);
 			} else {
@@ -190,30 +303,70 @@ void* clientHandling (void *arguments) {
 			}
 
 			
-			reply = malloc(sizeof(char) * 1024);
-			snprintf(reply, sizeof(reply), "%s%s%ld%s%s\n", replyStatus, "Content-Type: text/plain\r\nContent-Length: ", (strlen(outString) + 1), "\r\n\r\n", outString);
+			reply = malloc(sizeof(char)*  1024);
+			snprintf(reply, 1024, "%s%s%ld%s%s\n", replyStatus, "Content-Type: text/plain\r\nContent-Length: ", (strlen(outString) + 1), "\r\n\r\n", outString);
 			
-
 			bytes_sent = send(clientFd, reply, strlen(reply), 0);
 			free(reply);
 		} else {
 			if (requestLine[1].value[0] == '/') {
-				memmove(requestLine[1].value, requestLine[1].value+1, strlen(requestLine[1].value));
+				size_t len = strlen(requestLine[1].value);
+				if (len > 1) {  
+					memmove(requestLine[1].value, requestLine[1].value+1, len);
+				} else {
+					requestLine[1].value[0] = '\0';  
+				}
 			}
 				
-			FILE *targetFile;
-			targetFile = fopen(requestLine[1].value, "r");
+			FILE* targetFile;
+			const char* contentType;
+			if (requestLine[1].value != NULL) {
+				contentType = getFileType(requestLine[1].value);
+			} else {
+				contentType = "text/plain"; 
+			}
+
+			if (strcmp(contentType, "image/png") == 0 ||
+				strcmp(contentType, "image/jpeg") == 0 ||
+				strcmp(contentType, "image/gif") == 0 ||
+				strcmp(contentType, "application/pdf") == 0) 
+			{
+				targetFile = fopen(requestLine[1].value, "rb");
+				if (targetFile == NULL) {
+					printf("File does not exist. Returning status 404: %s\n", strerror(errno));
+					replyStatus = bulildStatusCodeReply(404, status_codes);
+					if (replyStatus) {
+						bytes_sent = send(clientFd, replyStatus, strlen(replyStatus), 0);
+						free(replyStatus);
+					}
+					exit;
+				}
+			} else {
+				targetFile = fopen(requestLine[1].value, "r");
+				if (targetFile == NULL) {
+					printf("File does not exist. Returning status 404: %s\n", strerror(errno));
+					replyStatus = bulildStatusCodeReply(404, status_codes);
+					if (replyStatus) {
+						bytes_sent = send(clientFd, replyStatus, strlen(replyStatus), 0);
+						free(replyStatus);
+					}
+					exit;
+				}
+			}
+
+			
+			
 
 			/*
 			TODO:
 			add dynamic buffer length
 
-				!! FROM STACKOVERFLOW !!
-				// in reality you would probably stat the file
-				// to find it's size and then malloc the memory
-				// or you could read the file twice:
-				// - first time counting the bytes
-				// - second time reading the bytes
+			!! FROM STACKOVERFLOW !!
+			// in reality you would probably stat the file
+			// to find it's size and then malloc the memory
+			// or you could read the file twice:
+			// - first time counting the bytes
+			// - second time reading the bytes
 						
 			*/
 			
@@ -223,33 +376,73 @@ void* clientHandling (void *arguments) {
 				bytes_sent = send(clientFd, replyStatus, strlen(replyStatus), 0);
 			} else {
 				if (status_codes[200]) {
-					long fileSize = geFileSize(targetFile);
-					char *outString = malloc(fileSize + 1);
+					size_t fileSize = getFileSize(targetFile);
+					char* outString = malloc(fileSize + 1);
+					if (!outString) {
+						printf("Memory allocation failed: %s \n", strerror(errno));
+						fclose(targetFile);
+						replyStatus = bulildStatusCodeReply(500, status_codes);
+						bytes_sent = send(clientFd, replyStatus, strlen(replyStatus), 0);
+						free(replyStatus);
+						exit;
+					}
   					memset (outString, 0, fileSize + 1);  
+					
 					//if (!outString) {
-					//	printf("Memory allocation failed: %s \n", ssrerror(errno));
+					//	printf("Memory allocation failed: %s \n", strerror(errno));
 					//}
-					char *current = outString;
-					int bytes, chunk=20;
+					char* current = outString;
+					int bytes, chunk=4096;
 
 					while ((bytes = fread(current, sizeof(char), chunk, targetFile)) > 0) {
 						current += bytes;
 					}
 					
-					outString[strlen(outString) - 1] = '\0';
+					//outString[strlen(outString) - 1] = '\0';
 					//printf("%s\n",outString);
 										
 					
 					replyStatus = bulildStatusCodeReply(200, status_codes);
-					reply = malloc(sizeof(char) * 16384);
 
-					snprintf(reply, sizeof(reply), "%s%s%ld%s%s\n", replyStatus, "Content-Type: application/octet-stream\r\nContent-Length: ", sizeof(outString), "\r\n\r\n", outString);
-					printf("%s\n", reply);
+					
 
-					bytes_sent = send(clientFd, reply, strlen(reply), 0);
+					//if (!reply) {
+					//	printf("Memory allocation failed: %s \n", strerror(errno));
+					//}
+
+					//sending the header first
+					char header[1024];
+					int headerLen = snprintf(header, sizeof(header), "%sContent-Type: %s\r\nContent-Length: %zu\r\n\r\n", replyStatus, contentType, fileSize);
+					
+					size_t totalSent = 0;
+					while (totalSent < headerLen) {
+						ssize_t sent = send(clientFd, header + totalSent, headerLen - totalSent, 0);
+						if (sent == -1) {
+							printf("Error: failed sending response header: %s\n", strerror(errno));
+							break;
+						}
+						totalSent += sent;
+					}
+
+
+					//snprintf(reply, strlen(outString) + 16384, "%s%s%s%s%zu%s%s\n", replyStatus, "Content-Type: ", contentType,"\r\nContent-Length: ", strlen(outString), "\r\n\r\n", outString);
+					//printf("%s\n", reply);
+
+
+					//bytes_sent = send(clientFd, reply, strlen(reply), 0);
+
+					totalSent = 0;
+					while (totalSent < fileSize) {
+						ssize_t sent = send(clientFd, outString + totalSent, fileSize - totalSent, 0);
+						if (sent == -1) {
+							printf("Error: failed sending file data: %s\n", strerror(errno));
+							break;
+						}
+						totalSent += sent;
+					}
+
 					//*outString = '\0';
 					free(outString);
-					free(reply);
 
 				} else {
 					replyStatus = strdup("HTTP/1.1 500 Internal Server Error\r\n\r\n");
@@ -262,9 +455,44 @@ void* clientHandling (void *arguments) {
 		
 		if (bytes_sent == -1) {
 			printf("Error sending server response: %s \n", strerror(errno));
-			exit(-1);
+			exit(1);
 		}
 
+	} else if (strcmp(requestLine[0].value, "POST") == 0 && strcmp(requestLine[1].value, "") != 0) {
+		int bytes_sent;
+		
+		//printf("folder and file: %s\n", requestLine[1].value);
+		//printf("POST data: %s\n", postData);
+
+		FILE *fptr;
+		const char* path = concatStrings("files/", requestLine[1].value);
+		fptr = fopen(path, "w");
+
+
+		if (fptr == NULL) {
+			printf("Error opening file: %s \n", strerror(errno));
+			replyStatus = bulildStatusCodeReply(500, status_codes);
+			bytes_sent = send(clientFd, replyStatus, strlen(replyStatus), 0);
+
+		} else {
+
+			fprintf(fptr, postData);
+
+			replyStatus = bulildStatusCodeReply(201, status_codes);
+			bytes_sent = send(clientFd, replyStatus, strlen(replyStatus), 0);
+			fclose(fptr);
+		}
+
+		
+
+		
+
+	} else {
+		printf("No target was specified in the http request");
+		int bytes_sent;
+		replyStatus = bulildStatusCodeReply(404, status_codes);
+		bytes_sent = send(clientFd, replyStatus, strlen(replyStatus), 0);
+		exit(1);
 	}
 	
 	
@@ -277,6 +505,9 @@ void* clientHandling (void *arguments) {
 
 	close(clientFd);
 
+	free(args->clientFd);
+    free(args);
+
 
 	return NULL;
 }
@@ -284,11 +515,11 @@ void* clientHandling (void *arguments) {
 /*void insertElement(struct requestElement headers[], int counter, char* key, char* value) {
 
 
-	//declaring **temp each iteration to dynamically alocate memory for the **strs array of strings
-	//using realloc to reallocate the **strs value and expand it by the size of the counter * the size of *char into the new **temp variable
-	char **temp = realloc(headers, (counter + 1) * sizeof(struct requestElement));
+	//declaring* *temp each iteration to dynamically alocate memory for the* *strs array of strings
+	//using realloc to reallocate the* *strs value and expand it by the size of the counter*  the size of* char into the new* *temp variable
+	char* *temp = realloc(headers, (counter + 1)*  sizeof(struct requestElement));
 
-	//error handling for **temp reallocation
+	//error handling for* *temp reallocation
 	if (temp == NULL){
 		fprintf(stderr, "Failed to allocate memory: %s \n", strerror(errno));
 		free(headers);
@@ -297,7 +528,7 @@ void* clientHandling (void *arguments) {
 
 	headers = temp;
 
-	//allocating uninitialized memory at the [counter] position of the **strs pointer to the size of token + 1 
+	//allocating uninitialized memory at the [counter] position of the* *strs pointer to the size of token + 1 
 	headers[counter] = malloc(sizeof(struct requestElement));
 
 	//error handing for allocating memory
@@ -312,7 +543,7 @@ void* clientHandling (void *arguments) {
 
 	struct requestElement temp = {key, value};
 
-	//coppying value of token into the [counter] element of the **strs array of strings
+	//coppying value of token into the [counter] element of the* *strs array of strings
 	memcpy(headers[counter], temp.key, sizeof temp.key);
 
 }
@@ -321,8 +552,11 @@ void* clientHandling (void *arguments) {
 
 int main() {
 	//filling array with status codes at startup
-	FILE *fptr;
+	FILE* fptr;
 	fptr = fopen("status_codes.txt", "r");
+
+	//TU NAREDI ERROR HANDLING ZA NEUSPESNO ODPRT FILE
+
 	char* status_codes[NUM_STATUS_CODES] = {NULL};
 	fillStatusArray(status_codes, fptr);
 	fclose(fptr);
@@ -362,7 +596,7 @@ int main() {
 									};
 	
 	//binding the socket
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+	if (bind(server_fd, (struct sockaddr* ) &serv_addr, sizeof(serv_addr)) != 0) {
 	 	printf("Bind failed: %s \n", strerror(errno));
 	 	return 1;
 	}
@@ -381,10 +615,13 @@ int main() {
 
 	int clientFd;
 	while (1) {
-		clientFd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+		clientFd = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
 		if (clientFd == -1) {
+			if (errno == EINTR) continue; 
+			perror("accept failed");
+			sleep(1); 
 			continue;
-		}
+		}	
 		printf("Client connected\n");
 
 		// Allocate memory for clientFd to prevent overwriting
